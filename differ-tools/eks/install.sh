26ccb130ca5707a8ca8cd1516eb3610f0e6678c8
Heechan Kim <heechan.dev@gmail.com>
2022-12-16 17:49:52 +0900

    Change script name

---
+++ differ-tools/eks/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT="diff-spec.sh"
+COMMAND="diff-spec"
+INSTALL_DIR="/usr/local/bin"
+
+if [[ ! -d $INSTALL_DIR ]]; then
+  mkdir -p $INSTALL_DIR
+fi
+
+# Copy Script and make soft link
+cp $SCRIPT $INSTALL_DIR
+
+if [[ -e $($INSTALL_DIR/$SCRIPT $INSTALL_DIR/$COMMAND) ]]; then
+	echo ""	
+else
+	ln -s $INSTALL_DIR/$SCRIPT $INSTALL_DIR/$COMMAND
+fi
+
+# Add Execute Permission
+chmod +x $INSTALL_DIR/$SCRIPT
+chmod +x $INSTALL_DIR/$COMMAND
+
+# Add Path to profile
+echo 'export PATH="$PATH":$INSTALL_DIR' >> ~/.profile
+source ~/.profile
