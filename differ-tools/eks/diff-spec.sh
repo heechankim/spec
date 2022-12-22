2ab43ae22940f99a1fa9f4c0e175d58e3651c40c
Heechan Kim <heechan.dev@gmail.com>
2022-12-22 12:25:23 +0900

    Update diff-spec script
    - Deployment Or Cronjob difference with option t

--- differ-tools/eks/diff-spec.sh
+++ differ-tools/eks/diff-spec.sh
@@ -4,44 +4,102 @@
 # target=
 # diff1=$(kubectl get deploy $target -o=jsonpath={.spec.template} | jq --sort-keys .)
 # diff2=$(yq <file> -o json | jq --sort-keys '.spec.template')
-
 #diff -c <(jq --sort-keys . prod_item-api.json) <(jq --sort-keys . prod2_item-api.json)
 
+ERROR='\033[1;91m' # Red
+RESULT='\033[0;101m' # Background Red
+INFO='\033[1;36m' # Cyan
+EOC='\033[0m' # End of Color
+
+RESOURCE_TYPE=
+K8S_NAMESPACE=
+
+function errorMessage {
+	echo -e "${ERROR}$1${EOC}"
+}
+
+function informMessage {
+	echo -e "${INFO}$1${EOC}"
+}
+
+function commandWithColorOutput {
+	echo -en "${INFO}"
+	echo -n "$1 "
+	eval $2
+	echo -en "${EOC}"
+}
+
+######################################################################################
+# Option settings
+######################################################################################
+while getopts "t:" opt;
+do
+	case $opt in
+	t)
+	    RESOURCE_TYPE=$OPTARG
+	    ;;
+#	n)
+#	    echo -e "option n"
+#	    K8S_NAMESPACE=$OPTARG
+#	    if [[ $K8S_NAMESPACE -eq "" ]]; then echo "Option Argument is missing"; exit -1; fi
+#	    ;;
+	?)
+	    exit -1;
+	esac
+done
+
+if [[ ! ("$RESOURCE_TYPE" == "deploy" || "$RESOURCE_TYPE" == "deployment" || "$RESOURCE_TYPE" == "cronjob") ]]; then 
+	errorMessage "Resource Type is invaild. Either deployment or cronjob"; 
+	exit -1; 
+fi
+
+shift $((OPTIND-1))
+
 if [[ -z $1 ]]; then
-	echo "1st argument is missing"
-	exit -1;
+        errorMessage "Old EKS Cluster name is missing"
+        exit -1;
 fi
 
 if [[ -z $2 ]]; then
-	echo "2nd argument is missing"
-	exit -1;
+        errorMessage "New EKS Cluster name is missing"
+        exit -1;
 fi
 
 if [[ -z $3 ]]; then
-	echo "3rd argument is missing"
-	exit -1;
+        errorMessage "Namespace is missing"
+        exit -1;
 fi
 
 if [[ -z $4 ]]; then
-	echo "4th argument is missing"
-	exit -1;
+        errorMessage "Resource is missing"
+        exit -1;
 fi
 
+OLD_CLUSTER=$1
+NEW_CLUSTER=$2
+NAMESPACE=$3
+TARGET=$4
 
-echo "====================================================================="
-aws eks update-kubeconfig --profile aws_mfa --region ap-northeast-2 --name $1
+######################################################################################
+# 1. Setting config file 
+# 2. Get Resource configuration
+# 3. Using Colordiff command
+######################################################################################
 
+informMessage "====================================================================="
 
-target1=$(kubectl get deploy $4 -n $3 -o=jsonpath={.spec.template})
+commandWithColorOutput "Old Cluster:" "aws eks update-kubeconfig --profile aws_mfa --region ap-northeast-2 --name $OLD_CLUSTER"
 
-aws eks update-kubeconfig --profile aws_mfa --region ap-northeast-2 --name $2
+target1=$(kubectl get $RESOURCE_TYPE $TARGET -n $NAMESPACE -o=jsonpath={.spec})
 
-echo ""
-echo "Namespace: $3"
-echo "Target: $4"
-echo "====================================================================="
+commandWithColorOutput "New Cluster:" "aws eks update-kubeconfig --profile aws_mfa --region ap-northeast-2 --name $NEW_CLUSTER"
 
-target2=$(kubectl get deploy $4 -n $3 -o=jsonpath={.spec.template})
+informMessage "Namespace: $NAMESPACE"
+informMessage "Resource: $RESOURCE_TYPE"
+informMessage "Target: $TARGET"
+informMessage "====================================================================="
+
+target2=$(kubectl get $RESOURCE_TYPE $TARGET -n $NAMESPACE -o=jsonpath={.spec})
 
 
 echo ""
